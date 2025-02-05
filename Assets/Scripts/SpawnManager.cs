0cbd657ec2ad7cd48c24439ffeaf93df80ace3fb
Ramazan Dagli <rdagliflutter@gmail.com>
2025-02-05 12:20:03 +0300

    SpawnManager cleaning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,17 +9,18 @@ public class SpawnManager : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
-        {
-            // get random x value between -20 , 20 for the spawn position
-            Vector3 _spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            // get random animal index
-            int _animalIndex = Random.Range(0, animalPrefabs.Length);
-
-            // spawn random animal on random position
-            Instantiate(animalPrefabs[_animalIndex], _spawnPos, animalPrefabs[_animalIndex].transform.rotation);
-        }
+            SpawnRandomAnimal();
     }
 
+    private void SpawnRandomAnimal()
+    {
+        // get random x value between -20 , 20 for the spawn position
+        Vector3 _spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
+        // get random animal index
+        int _animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        // spawn random animal on random position
+        Instantiate(animalPrefabs[_animalIndex], _spawnPos, animalPrefabs[_animalIndex].transform.rotation);
+    }
 }
