3e4e35e2505a8d2a81689911e3f47327b35640cc
Ramazan Dagli <rdagliflutter@gmail.com>
2025-02-05 12:18:25 +0300

    v0.7 Spawn Manager
    Spawning on random position random animals

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float spawnRangeX = 15f;
+    public float spawnPosZ = 20f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // get random x value between -20 , 20 for the spawn position
+            Vector3 _spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            // get random animal index
+            int _animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            // spawn random animal on random position
+            Instantiate(animalPrefabs[_animalIndex], _spawnPos, animalPrefabs[_animalIndex].transform.rotation);
+        }
+    }
+
+
+}
